1310a690ca14d4e587efdc4d562438a1aa8aada9
Rohit <rk02342n@pace.edu>
2026-02-11 23:57:29 -0500

    Footer color

--- checkpointer-frontend/src/routes/__root.tsx
+++ checkpointer-frontend/src/routes/__root.tsx
@@ -14,7 +14,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 
 function Footer() {
   return(
-    <footer className="border-4 border-border bg-secondary py-8 sm:py-12 mt-12 mx-4 shadow-[4px_4px_0px_0px_rgba(41,37,36,1)] dark:shadow-[4px_4px_0px_0px_rgba(120,113,108,0.5)] sm:shadow-[8px_8px_0px_0px_rgba(41,37,36,1)] dark:sm:shadow-[8px_8px_0px_0px_rgba(120,113,108,0.5)] mb-10">
+    <footer className="border-4 border-border bg-black py-8 sm:py-12 mt-12 mx-4 shadow-[4px_4px_0px_0px_rgba(41,37,36,1)] dark:shadow-[4px_4px_0px_0px_rgba(120,113,108,0.5)] sm:shadow-[8px_8px_0px_0px_rgba(41,37,36,1)] dark:sm:shadow-[8px_8px_0px_0px_rgba(120,113,108,0.5)] mb-10">
         <div className="mx-auto px-6 sm:px-12 flex flex-col md:flex-row justify-between items-center gap-4">
           <div className="flex items-center gap-2">
             <span className="font-bold text-foreground font-alt">Checkpointer © 2026</span>
